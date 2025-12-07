48fa6f2ba9f5acb928bdfe4b32710947718e86e7
rdefranc-hub <rdefranc@gmail.com>
2025-12-07 12:21:49 -0300

    Fix PostCSS + Tailwind plugin for Cloudflare

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,9 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
 export default config;
