8bdb9526ac1943ad779536491123cc43fbf370d8
rdefranc-hub <rdefranc@gmail.com>
2025-12-07 10:06:36 -0300

    Fix: Tailwind PostCSS plugin for Next.js 16

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,5 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import tailwindcssPostcss from "@tailwindcss/postcss";
-
 export default {
   plugins: {
-    [tailwindcssPostcss]: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 };
