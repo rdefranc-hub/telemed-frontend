fe17f345dc28e32ea4c0d1b83484bc9d554ded0e
rdefranc-hub <rdefranc@gmail.com>
2025-12-07 09:33:54 -0300

    Fix Tailwind PostCSS plugin for Cloudflare

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,10 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import tailwindcssPostcss from "@tailwindcss/postcss";
+
 export default {
   plugins: {
-    tailwindcss: {},
+    [tailwindcssPostcss]: {},
     autoprefixer: {},
   },
 };
